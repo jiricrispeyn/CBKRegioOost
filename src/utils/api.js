fe0dfb753d1fe18362b1ae205e871e076a400479
Jiri Crispeyn <jiri.crispeyn@gmail.com>
2017-08-27 16:33:27 +0200

    Setup api

---
+++ src/utils/api.js
@@ -0,0 +1,85 @@
+const apiUrl = 'https://cbkregio-oost.herokuapp.com';
+
+const api = {
+  getLeagues: getLeagues,
+  getAddresses: getAddresses,
+  getPlayers: getPlayers,
+  getPlayerRankings: getPlayerRankings,
+  getPlayerDetail: getPlayerDetail,
+  getTrophies: getTrophies
+};
+
+function getLeagues() {
+  const url = `${apiUrl}/leagues`;
+  
+  return fetch(url, { method: 'GET'}).then(res => {
+    if (!res.ok) {
+      throw new Error();
+    }
+
+    return res.json();
+  });
+}
+
+function getAddresses(league) {
+  const url = `${apiUrl}/addresses/${league}`;
+
+  return fetch(url, { method: 'GET'}).then(res => {
+    if (!res.ok) {
+      throw new Error();
+    }
+
+    return res.json();
+  });
+}
+
+function getPlayers(league) {
+  const url = `${apiUrl}/players/${league}`;
+  
+  return fetch(url, { method: 'GET'}).then(res => {
+    if (!res.ok) {
+      throw new Error();
+    }
+
+    return res.json();
+  });
+}
+
+function getPlayerRankings(league) {
+    const url = `${apiUrl}/playerrankings/${league}`;
+    
+    return fetch(url, { method: 'GET'}).then(res => {
+      if (!res.ok) {
+        console.log(res);
+        throw new Error(res);
+      }
+  
+      return res.json();
+    });
+}
+
+function getPlayerDetail(id) {
+    const url = `${apiUrl}/player/${id}`;
+    
+    return fetch(url, { method: 'GET'}).then(res => {
+      if (!res.ok) {
+        throw new Error();
+      }
+  
+      return res.json();
+    });
+}
+
+function getTrophies() {
+  const url = `${apiUrl}/trophies`;
+  
+  return fetch(url, { method: 'GET'}).then(res => {
+    if (!res.ok) {
+      throw new Error();
+    }
+
+    return res.json();
+  });
+}
+
+module.exports = api;
