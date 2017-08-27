7606b0607790393bb28710e4113b94fe319635d6
Jiri Crispeyn <jiri.crispeyn@gmail.com>
2017-08-27 16:35:39 +0200

    Create Leagues component

---
+++ src/screens/LeaguesDetail.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import { StyleSheet } from 'react-native';
+import { Container, Content, Text } from 'native-base';
+
+class LeaguesDetail extends Component {
+  state = {  }
+  render() {
+    return (
+      <Container>
+        <Content padder>
+          <Text>LeaguesDetail</Text>
+        </Content>
+      </Container>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+
+});
+
+export default LeaguesDetail;
